a9610c85d209abca5fe81a8c83dcd39ab0b3d62d
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-22 05:10:41 +0500

    20 valid parentheses

---
+++ 000020_valid_parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        closed = {
+                ')': '(',
+                '}': '{',
+                ']': '['
+        }
+        expected_parens = []
+        
+        for char in s:
+            if char in closed:
+                if expected_parens and expected_parens[-1] == closed[char]:
+                    expected_parens.pop()
+                    continue
+                return False
+            
+            expected_parens.append(char)
+        
+        return len(expected_parens) == 0
