0218fd624e5244f21346fd72548ecc0aa4055d03
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-22 06:11:02 +0500

    150 eval reverse polish notation

---
+++ 000150_eval_rpn.py
@@ -0,0 +1,26 @@
+# PYTHON 3 SOLUTION
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        eval_values = []
+        for token in tokens:        
+            if token == "+":
+                b, a = eval_values.pop(), eval_values.pop()
+                eval_values.append(a + b)
+            elif token == "-":
+                b, a = eval_values.pop(), eval_values.pop()
+                eval_values.append(a - b)
+            elif token == "*":
+                b, a = eval_values.pop(), eval_values.pop()
+                eval_values.append(a * b)
+            elif token == "/":
+                b, a = eval_values.pop(), eval_values.pop()
+                eval_values.append(int(a / b))
+            else:
+                eval_values.append(int(token))
+
+        return eval_values[0]
