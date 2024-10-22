132ca33f2e668486e9941d61ece0dd7a5549e4fe
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-22 05:39:02 +0500

    155 min stack

---
+++ 000155_min_stack.py
@@ -0,0 +1,34 @@
+class MinStack(object):
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        self.minStack.append(
+                min(val, self.minStack[-1] if self.minStack else val)
+                )
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.minStack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
