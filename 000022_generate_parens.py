b3050942f8eebd74a8d2c5e22b400b1b67e7e4fd
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-23 17:00:20 +0500

    22 generate perenthesis

---
+++ 000022_generate_parens.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack, res = [], []
+
+        def genParens(openN, closeN):
+            if openN == closeN == n:
+                res.append(''.join(stack))
+                return
+
+            if openN < n:
+                stack.append('(')
+                genParens(openN + 1, closeN)
+                stack.pop()
+
+            if closeN < openN:
+                stack.append(')')
+                genParens(openN, closeN + 1)
+                stack.pop()
+
+
+        genParens(0, 0)
+        return res
